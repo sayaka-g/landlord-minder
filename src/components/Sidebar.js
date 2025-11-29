a7c124ef0722ac44e60e958d407fc26ce25b687a
s.g <berwick.st.sg@gmail.com>
2025-11-29 19:45:35 +0000

    Add logout functionality

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -9,96 +9,105 @@ import ReceiptLongIcon from '@mui/icons-material/ReceiptLong';
 import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import LogoutIcon from '@mui/icons-material/Logout';
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
 
 const Sidebar = () => {
-  return (
-    <Drawer
-        variant="permanent"
-        sx={{
-          flexShrink: 0
-        }}
-      >
-        <Toolbar />
-        <Box sx={{ overflow: 'auto' }}>
-            <List>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <BedroomChildIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Rooms" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <PersonIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Tenants" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <DateRangeIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Stays" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <ElectricBoltIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Bills" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <TextSnippetIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Reports" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <ReceiptLongIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Invoices" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <AccountBalanceWalletIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Balance" />
-                    </ListItemButton>
-                </ListItem>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <CalendarMonthIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Schedule" />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-            <Divider />
-            <List>
-                <ListItem>
-                    <ListItemButton>
-                        <ListItemIcon>
-                            <LogoutIcon />
-                        </ListItemIcon>
-                        <ListItemText primary="Logout" />
-                    </ListItemButton>
-                </ListItem>
-            </List>
-        </Box>
-    </Drawer>
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        await signOut(auth);
+        navigate("/");
+    };
+    return (
+        <Drawer
+            variant="permanent"
+            sx={{
+            flexShrink: 0
+            }}
+        >
+            <Toolbar />
+            <Box sx={{ overflow: 'auto' }}>
+                <List>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <BedroomChildIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Rooms" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <PersonIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Tenants" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <DateRangeIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Stays" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ElectricBoltIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Bills" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <TextSnippetIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Reports" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <ReceiptLongIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Invoices" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <AccountBalanceWalletIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Balance" />
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem>
+                        <ListItemButton>
+                            <ListItemIcon>
+                                <CalendarMonthIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Schedule" />
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+                <Divider />
+                <List>
+                    <ListItem>
+                        <ListItemButton color="inherit" onClick={handleLogout}>
+                            <ListItemIcon>
+                                <LogoutIcon />
+                            </ListItemIcon>
+                            <ListItemText primary="Logout" />
+                        </ListItemButton>  
+                    </ListItem>
+                </List>
+            </Box>
+        </Drawer>
   );
 };
 
